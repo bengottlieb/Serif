d523ec3ff762641bc136c5edc70162c3050a225b
Ben Gottlieb <ben@standalone.com>
2017-06-04 16:10:40 -0500

    Initial Commit

---
+++ Serif/Serif.h
@@ -0,0 +1,19 @@
+//
+//  Serif.h
+//  Serif
+//
+//  Created by Ben Gottlieb on 6/4/17.
+//  Copyright © 2017 Stand Alone, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Serif.
+FOUNDATION_EXPORT double SerifVersionNumber;
+
+//! Project version string for Serif.
+FOUNDATION_EXPORT const unsigned char SerifVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Serif/PublicHeader.h>
+
+
