9b4572a30164c189d006b3f9c623206b561161bf
Ben Gottlieb <ben@standalone.com>
2017-06-06 08:51:23 -0500

    added Label view

---
+++ TestHost/Views/LabelView.swift
@@ -0,0 +1,14 @@
+//
+//  LabelView.swift
+//  Serif
+//
+//  Created by Ben Gottlieb on 6/6/17.
+//  Copyright © 2017 Stand Alone, Inc. All rights reserved.
+//
+
+import Foundation
+import CrossPlatformKit
+
+class LabelView: UXView {
+	
+}
